f750c3268b484893318ce0a6ea04eab17d2ce1a7
relkabou <elkaboussi@pm.me>
2023-04-11 03:47:42 +0000

    add textures

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -1,5 +1,7 @@
 #include "srcs.h"
+#include <stdbool.h>
 
+bool	is_texture_valid(void *mlx, char *path);
 bool	is_filename_valid(char *filename);
 
 bool	is_filename_valid(char *filename)
@@ -16,6 +18,20 @@ bool	is_filename_valid(char *filename)
 	filename += filename_len - extension_len;
 	if (strcmp(filename, extension))
 		return (false);
+	return (true);
+}
+
+bool	is_texture_valid(void *mlx, char *path)
+{
+	int		img_height;
+	int		img_width;
+	void	*img;
+
+	img = mlx_xpm_file_to_image(mlx, path, &img_width, &img_height);
+	if (img == NULL) {
+		return (false);
+	}
+	free(img);
 	return (true);
 }
 
