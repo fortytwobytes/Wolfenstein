a78cd488391b6168855f7f9194dca7e004aada55
Othmane Nouakchi <onouakch@e1r4p13.1337.ma>
2023-05-18 17:14:27 +0100

    bugfix: memory leaks in empty file

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: relkabou <relkabou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: onouakch <onouakch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 17:09:19 by onouakch          #+#    #+#             */
-/*   Updated: 2023/05/08 01:45:21 by relkabou         ###   ########.fr       */
+/*   Updated: 2023/05/18 17:12:03 by onouakch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/srcs.h"
 
-// TODO: add mlx_terminate later
-int32_t main(int32_t argc, const char *argv[]) {
-	t_var var;
+int32_t	main(int32_t argc, const char *argv[])
+{
+	t_var	var;
 
 	if (argc != 2)
 		fatal("Usage: ./cub3D <map.cub>\n");
@@ -24,13 +24,20 @@ int32_t main(int32_t argc, const char *argv[]) {
 	mlx_loop_hook(var.mlx, move_hook, &var);
 	mlx_loop_hook(var.mlx, draw_hook, &var);
 	mlx_loop(var.mlx);
+	mlx_terminate(var.mlx);
 	return (EXIT_SUCCESS);
 }
 
 void	end_game(t_var *var)
 {
+	int	i;
+
+	i = -1;
 	free_split(var->map.map);
 	mlx_delete_image(var->mlx, var->image);
 	mlx_close_window(var->mlx);
+	while (++i < SCREEN_HEIGHT)
+		free(var->texture.buffer[i]);
+	free(var->texture.buffer);
 	exit(0);
 }
